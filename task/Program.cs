672db3b350340c84ff7132e99194786b622e032e
Michail <paginmichail@gmail.com>
2022-11-30 00:56:53 +0300

    commit message

---
+++ task/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] array)
+        {
+            string[] arr;
+            int count;
+            string n;
+            string[] result; 
+        }
+    }
+}
